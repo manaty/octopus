d62f61b20f3b60e722f0b736e85f43645b06fd02
Andrei Tomashpolskiy <nordmann89@gmail.com>
2019-06-14 10:36:11 +0300

    Configurable threshold and min/max number of samples per round for client and master time sync

--- server/src/main/java/net/manaty/octopusync/service/sync/SyncRound.java
+++ server/src/main/java/net/manaty/octopusync/service/sync/SyncRound.java
@@ -12,12 +12,11 @@ import java.util.function.Consumer;
 public class SyncRound<R> {
     private static final Logger LOGGER = LoggerFactory.getLogger(SyncRound.class);
 
-    private static final double STDDEV_THRESHOLD = 1.0;
-    private static final int MIN_SAMPLES_BEFORE_SUCCESS = 10;
-    private static final int MAX_SAMPLES_BEFORE_FAILURE = 100;
-
     private final SyncResultBuilder<?, R> resultBuilder;
     private final Consumer<R> resultConsumer;
+    private final double devThreshold;
+    private final int minSamples;
+    private final int maxSamples;
     private final AtomicLong seqnum;
 
     private final MeanVarianceSampler sampler;
@@ -25,17 +24,33 @@ public class SyncRound<R> {
     private volatile Consumer<SyncTimeRequest> requestConsumer;
     private volatile long sent;
 
-    public SyncRound(SyncResultBuilder<?, R> resultBuilder, Consumer<R> resultConsumer) {
+    /**
+     * @param devThreshold Standard deviation threshold;
+     *                     if the result is smaller than this value, it's considered a success
+     * @param minSamples Minimum number of samples to make
+     * @param maxSamples Maximum number of samples to make
+     */
+    public SyncRound(
+            SyncResultBuilder<?, R> resultBuilder,
+            Consumer<R> resultConsumer,
+            double devThreshold,
+            int minSamples,
+            int maxSamples) {
+
         this.resultBuilder = resultBuilder;
         this.resultConsumer = resultConsumer;
+        this.devThreshold = devThreshold;
+        this.minSamples = minSamples;
+        this.maxSamples = maxSamples;
         this.seqnum = new AtomicLong(0);
         this.sampler = new MeanVarianceSampler();
     }
 
-    public void handleResponse(SyncTimeResponse response) throws Exception {
+    public void handleResponse(SyncTimeResponse response) {
         long seqnum = this.seqnum.get();
         if (seqnum != response.getSeqnum()) {
-            throw new IllegalStateException("seqnum does not match");
+            throw new IllegalStateException("seqnum does not match value in other party's response" +
+                    " (" + seqnum + " <> " + response.getSeqnum() + ")");
         } else {
             long received = response.getReceivedTimeUtc();
             long delta = received - sent;
@@ -46,11 +61,11 @@ public class SyncRound<R> {
                         resultBuilder.getRound(), seqnum, resultBuilder.getTargetDescription(),
                         sampler.getMean(), sampler.getVarianceUnbiased(), stddev));
             }
-            if (seqnum > MIN_SAMPLES_BEFORE_SUCCESS && stddev < STDDEV_THRESHOLD ) {
+            if (seqnum > minSamples && stddev < devThreshold) {
                 resultConsumer.accept(resultBuilder.ok(System.currentTimeMillis(), delta));
-            } else if (seqnum == MAX_SAMPLES_BEFORE_FAILURE) {
+            } else if (seqnum == maxSamples) {
                 String message = String.format("failed to sync with %s in %d round-trips; stddev is greater than %.2f",
-                        resultBuilder.getTargetDescription(), seqnum, STDDEV_THRESHOLD);
+                        resultBuilder.getTargetDescription(), seqnum, devThreshold);
                 LOGGER.error(message);
                 resultConsumer.accept(resultBuilder.failure(System.currentTimeMillis(), new IllegalStateException(message)));
             } else {
