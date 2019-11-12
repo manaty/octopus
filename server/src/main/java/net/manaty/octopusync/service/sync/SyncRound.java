54a442511506a4a71c05666db4a188c2a6e114e0
Andrei Tomashpolskiy <nordmann89@gmail.com>
2019-11-12 20:55:42 +0300

    Update time synchronization

--- server/src/main/java/net/manaty/octopusync/service/sync/SyncRound.java
+++ server/src/main/java/net/manaty/octopusync/service/sync/SyncRound.java
@@ -63,7 +63,7 @@ public class SyncRound<R> {
                         sampler.getMean(), sampler.getVarianceUnbiased(), stddev));
             }
             if (seqnum > minSamples && stddev < devThreshold) {
-                resultConsumer.accept(resultBuilder.ok(System.currentTimeMillis(), Math.round(delta)));
+                resultConsumer.accept(resultBuilder.ok(System.currentTimeMillis(), Math.round(sampler.getMean())));
             } else if (seqnum == maxSamples) {
                 String message = String.format("failed to sync with %s in %d round-trips; stddev is greater than %.2f",
                         resultBuilder.getTargetDescription(), seqnum, devThreshold);
